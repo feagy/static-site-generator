eeb883d445c48a2a4cb3878eee5a5e9514354c00
feagy <ferhat1903dnz@gmail.com>
2026-08-02 16:05:22 +0300

    TextNode added

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    text_node_test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(text_node_test)
+
+if __name__ == "__main__":
+    main()
